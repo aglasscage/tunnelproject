177bc787c60dc02ab3c658cb406b39cbf3f7d606
aglasscage <sakasmann1@cougars.ccis.edu>
2015-09-30 00:33:02 -0400

    updated the ship physics

--- includes/Sub.h
+++ includes/Sub.h
@@ -10,7 +10,7 @@ class Sub
 {
 public:
 	Sub()
-		: alive_(true), dy(0)
+		: alive_(true), dy(0), momentum_(0)
 	{
 		subTexture = load_texture("includes/sub.png");
 		subRect.x = W / 2 - SUB_WIDTH / 2;
@@ -19,7 +19,18 @@ public:
 		subRect.w = SUB_WIDTH;
 	}
 	int getSuby() const { return subRect.y; }
-	
+	void incMomentum()
+	{
+		if (momentum_ < 15) momentum_++;
+	}
+	void decMomentum()
+	{
+		if (momentum_ > 0) momentum_--;
+	}
+	int getMomentum() const
+	{
+		return momentum_;
+	}
 	void setSuby(int newY)
 	{
 		subRect.y = newY;			
@@ -39,26 +50,62 @@ public:
 	void movement()
 	{
 		const Uint8 * keystate = SDL_GetKeyboardState(NULL);
-   	 	if(keystate[SDL_SCANCODE_SPACE])
+
+		if(keystate[SDL_SCANCODE_SPACE])
     	{
-    		dy = -3;
+    		incMomentum();
+    		if (getMomentum() < 4)
+    		{
+    			dy = -0;
+    		}
+    		else if (getMomentum() < 8)
+    		{
+    			dy = -1;
+    		}
+    		else if (getMomentum() < 12)
+    		{
+    			dy = -2;
+    		}
+    		else
+    		{
+    			dy = -3;
+    		}
     	}
 	    else
 	    {
+	    	decMomentum();
+	    	if (getMomentum() >= 12)
+	    	{
+	    		dy = -3;
+	    	}
+	    	else if (getMomentum() >= 8)
+	    	{
+	    		dy = -2;
+	    	}
+	    	else if (getMomentum() >= 4)
+	    	{
+	    		dy = -1;
+	    	}
+	    	else if (getMomentum() > 0)
+	    	{
+	    		dy = 0;
+	    	}
+	    	else
+	    	{
+	    		// Speed of the ship while it is sinking.
+	    		dy = 2;
+	    	}
 	    }
-	    if (dy <= 1)
-		{
-			dy++;
-		}
 	    subRect.y += dy;
 	}
 	void moveSub()		{ subRect.y += dy; } 
 //private:
 	SDL_Texture* subTexture = NULL;
 	SDL_Rect subRect;
-	int dy;
 private:
+	int dy;
 	bool alive_;
+	int momentum_;
 };
 
 #endif
